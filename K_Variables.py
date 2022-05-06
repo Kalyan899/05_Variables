07ccecd8a4a9ee251f6c44c3d0f079783ac26886
Kalyan899 <105037610+Kalyan899@users.noreply.github.com>
2022-05-06 17:16:42 +0530

    Add files via upload

---
+++ K_Variables.py
@@ -0,0 +1,33 @@
+"""
+Name: Kalyan
+Date & Day: 06-05-2022 & Friday
+File Description: Variables
+"""
+
+"""Variable can be used in Starting, Middle and Ending"""
+# a=4
+# _a1=8
+# _a=16
+# print(a)
+# print(_a1)
+# print(_a)
+
+"""Numbers can be used in the Middel and Ending"""
+# a1a=4
+# a1=8
+# print('a1a')
+# print("a1")
+
+"""Underscore can be used in Starting, Middle and Ending """
+# _a=4
+# a_a=8
+# a_=16
+# print("_a")
+# print('''a_a''')
+# print("a_")
+
+'''To check the variable True or False'''
+print('Kalyan'.isidentifier())
+print('8Kalyan'.isidentifier())
+print('_Kalyan'.isidentifier())
+
